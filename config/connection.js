66922154dddd279d32da7c67846e2dec2b5dfb06
moliveros <mcoliveros1202@gmail.com>
2022-07-30 19:44:06 -0500

    initial commit, files setup, package install

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+let sequelize;
+
+if (process.env.JAWSBD_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
+    host: 'localhost',
+    dialect: 'mysql',
+    password: null,
+    port: 3306
+  });
+}
+module.exports = sequelize;
