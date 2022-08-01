c67a643a5ab66c22ddbd3faf7cbc32d85d99fdca
moliveros <mcoliveros1202@gmail.com>
2022-08-01 10:22:09 -0500

    created Comment model and comment-routes; tested in Insomnia

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('sequelize');
+const { Post, User, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
     Post.findAll({
+        order: [['created_at', 'DESC']],
         // Query configuration
         attributes: ['id', 'content', 'title', 'created_at'],
-        order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
