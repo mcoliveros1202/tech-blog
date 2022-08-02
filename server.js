9c1b1d0c3a3721af964c30eb1f18314d0af6fdd2
moliveros <mcoliveros1202@gmail.com>
2022-08-02 11:25:39 -0500

    created homepage

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 // const session = require('express-session');
-// const exphbs = require('express-handlebars');
-// const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,8 +28,8 @@ const PORT = process.env.PORT || 3001;
 
 // const hbs = exphbs.create ({ helpers });
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
